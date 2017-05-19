773365ac53c8a7e0ba3dc63b03e9c5c028d0f495
Michał Łyszczek <michal.lyszczek@gmail.com>
2017-05-20 01:06:15 +0200

    Added mtest v0.0.1

--- mtest.h
+++ mtest.h
@@ -3,7 +3,46 @@
     Author: Michał Łyszczek <michal.lyszczek@bofc.pl>
    ========================================================================== */
 
+/* ==== mtest version v0.0.1 ================================================ */
+
+
+/* ==== include files ======================================================= */
+
+
 #include <stdio.h>
+
+/* ==== private macros ====================================================== */
+
+
+/* ==========================================================================
+    all printf will be able to print back to stderr and stdout
+   ========================================================================== */
+
+
+#define mt_allow_print()                                                       \
+    freopen("/dev/tty", "w", stderr);                                          \
+    freopen("/dev/tty", "w", stdout)
+
+
+/* ==========================================================================
+    when this macro is called, all printing to stderr and stdout will have no
+    effect. To reenable printing, call mt_allow_print
+   ========================================================================== */
+
+
+#define mt_disable_print()                                                     \
+    fclose(stderr);                                                            \
+    fclose(stdout)
+
+
+/* ==== public macros ======================================================= */
+
+
+/* ==========================================================================
+    macro with definitions, call this macro no more and no less than ONCE in
+    global scope. Its task is to define some variables used by mtest macros.
+   ========================================================================== */
+
 
 #define mt_defs()                                                              \
     const char *curr_test;                                                     \
@@ -14,15 +53,13 @@
     int mt_total_asserts = 0;                                                  \
     int mt_succed_asserts = 0
 
-#define mt_allow_print()                                                       \
-    freopen("/dev/tty", "w", stderr);                                          \
-    freopen("/dev/tty", "w", stdout)
 
-#define mt_disable_print()                                                     \
-    fclose(stderr);                                                            \
-    fclose(stdout)
+/* ==========================================================================
+    macro runs test 'f'. 'f' is just a function (without parenthesis ()).
+   ========================================================================== */
 
-#define mt_run(f)                                                              \
+
+#define mt_run(f) do {                                                         \
     curr_test = #f;                                                            \
     mt_test_status = 0;                                                        \
     ++mt_total_tests;                                                          \
@@ -36,9 +73,16 @@
     {                                                                          \
         fprintf(stdout, "[OK]\n");                                             \
         ++mt_succed_tests;                                                     \
-    }
+    } } while(0)
 
-#define mt_assert(e)                                                           \
+
+/* ==========================================================================
+    simple assert, when expression 'e' is evaluated to false, assert message
+    will be logged, and macro will force function to return
+   ========================================================================== */
+
+
+#define mt_assert(e) do {                                                      \
     ++mt_total_asserts;                                                        \
     if (!(e))                                                                  \
     {                                                                          \
@@ -49,9 +93,16 @@
         mt_test_status = -1;                                                   \
         return;                                                                \
     }                                                                          \
-    ++mt_succed_asserts
+    ++mt_succed_asserts; } while (0)
 
-#define mt_fail(e)                                                             \
+
+/* ==========================================================================
+    same as mt_assert, but function is not forced to return, and test can
+    continue
+   ========================================================================== */
+
+
+#define mt_fail(e) do {                                                        \
     ++mt_total_asserts;                                                        \
     if (!(e))                                                                  \
     {                                                                          \
@@ -61,10 +112,17 @@
         mt_fail_flag = 1;                                                      \
         mt_test_status = -1;                                                   \
     } else                                                                     \
-        ++mt_succed_asserts
+        ++mt_succed_asserts; } while(0)
 
-#define mt_return()                                                            \
+
+/* ==========================================================================
+    prints tests summary and returns with 0 if all tests have passed, or -1
+    when at least one test has failed
+   ========================================================================== */
+
+
+#define mt_return() do {                                                       \
     printf("summary: %d/%d tests passed, %d/%d asserts passed\n",              \
          mt_succed_tests, mt_total_tests, mt_succed_asserts, mt_total_asserts);\
-    return mt_fail_flag
+    return mt_fail_flag; } while(0)
 
